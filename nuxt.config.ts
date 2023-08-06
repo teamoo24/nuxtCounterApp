cb190c774d47c456037b81e50074bf8ea5d64d12
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-06 21:45:44 +0900

    full static generateテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
-export default {
-  target: 'static',
-  generate: {subFolders: true},
-  router: {
-    base: '/nuxtCounterApp/'
-  }
-}
+import axios from 'axios'
+
+// nuxt.config.ts
+export default defineNuxtConfig({
+  hooks: {
+    async "nitro:config"(nitroConfig) {
+      if (nitroConfig.dev) {
+        return;
+      }
+      const res = await axios.get("https://api.nuxtjs.dev/mountains");
+      if (nitroConfig.prerender?.routes === undefined) {
+        return;
+      }
+      nitroConfig.prerender.routes = res.data.map((mount: any) => {
+        return `/mountains/${mount.slug}`;
+      });
+    },
+  },
+});
