187b714846e8b9eca93bc10698bbc92cb2412fca
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-06 21:48:47 +0900

    テスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,7 @@
-import axios from 'axios'
-
-// nuxt.config.ts
-export default defineNuxtConfig({
-  hooks: {
-    async "nitro:config"(nitroConfig) {
-      if (nitroConfig.dev) {
-        return;
-      }
-      const res = await axios.get("https://api.nuxtjs.dev/mountains");
-      if (nitroConfig.prerender?.routes === undefined) {
-        return;
-      }
-      nitroConfig.prerender.routes = res.data.map((mount: any) => {
-        return `/mountains/${mount.slug}`;
-      });
-    },
-  },
-});
+export default {
+  target: 'static',
+  generate: {subFolders: true},
+  router: {
+    base: '/nuxtCounterApp/'
+  }
+}
